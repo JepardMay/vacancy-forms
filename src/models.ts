982d6bd361f9c91caf2e3cb2850ab922c5d906f7
Veronika Palto <nikalalin@gmail.com>
2025-02-24 17:59:02 +0300

    Create mock json and fetch function

---
+++ src/models.ts
@@ -0,0 +1,25 @@
+export interface ApplicationData {
+  id?: number;
+  jobTitle: string;
+  vacancyName: string;
+  department: string;
+  openingDate: string;
+  plannedClosingDate: string;
+  gender: 'Мужской' | 'Женский';
+  education: 'Высшее' | 'Среднее';
+  salary?: {
+    type: 'На руки' | 'До вычета налогов';
+    from?: number;
+    to?: number;
+  };
+  region: string;
+  address: string;
+  metroStation?: string;
+  workExperience: string;
+  workSchedule: 'Полный день' | 'Сменный 5/2' | 'Сменный 2/2';
+  employmentType: 'Полная занятость' | 'Частичная занятость' | 'Стажировка';
+  responsibilities?: string;
+  candidateRequirements?: string;
+  advantages?: string;
+  weOffer?: string;
+};
